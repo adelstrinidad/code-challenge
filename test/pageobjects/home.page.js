5e5ae2355056f6d029cd7e9dfa53c2908160538a
Adelquis <adelquis.trinidad@gmail.com>
2020-10-22 11:55:09 -0300

    applying formatting and eslint to the code

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -4,21 +4,19 @@ import Page from './page';
  * sub page containing specific selectors and methods for a specific page
  */
 class HomePage extends Page {
-    /**
+  /**
      * define selectors using getter methods
      */
-    get searchKeyBox () { return $('#search-key') }
+  get searchKeyBox() {return $('#search-key');}
 
 
-    /**
+  /**
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password
      */
-    searchItem (item) {
-        //add method
-    }
-
-
+  searchItem(item) {
+    // add method
+  }
 }
 
 export default new HomePage();
--- test/specs/aliexpress.spec.js
+++ test/specs/aliexpress.spec.js
@@ -1,48 +1,45 @@
-import HomePage from '../pageobjects/home.page'
+import HomePage from '../pageobjects/home.page';
 
 describe('Aliexpress test suite', () => {
-    it('Verify availability for a specific product', () => {
-        HomePage.open();
-        const searchKey = $('#search-key')
-        searchKey.waitForExist({timeout: 5000})
-        searchKey.setValue('iphone')
-        browser.keys(['Enter'])
+  it('Verify availability for a specific product', () => {
+    HomePage.open();
+    const searchKey = $('#search-key');
+    searchKey.waitForExist({timeout: 5000});
+    searchKey.setValue('iphone');
+    browser.keys(['Enter']);
 
-        try{
-            $('.next-dialog-body').waitForExist({ timeout:2000});
-        }catch{
-            console.log('the banner is not being displayed')
-        }
-        
-        $('.next-dialog-close').click();
-        
+    try {
+      $('.next-dialog-body').waitForExist({timeout: 2000});
+    } catch (e) {
+      console.log(e);
+    }
 
-        //browser.pause(3000)
-        browser.execute(function (elSelector) {
-            window.scrollBy(0,5000)
-        })
-
-        browser.waitUntil(
-            () => $('.next-pagination-list').isExisting(),
-            {
-                timeout: 5000,
-                timeoutMsg: 'expected text to be different after 5s'
-            }
-        );
-
-        const nextButtons = $$('.next-pagination-list .next-btn')
-        nextButtons[1].click()
-        const listItems = $$('.list-item')
-        listItems[1].click()
-        browser.pause(3000)
-        const windowHandle = browser.getWindowHandles()
-        browser.switchToWindow(windowHandle[1])
-        browser.pause(3000)
-        console.log($('.product-quantity-tip span').getText())
+    $('.next-dialog-close').click();
 
 
-        
+    // browser.pause(3000)
+    browser.execute(function(elSelector) {
+      window.scrollBy(0, 5000);
     });
+
+    browser.waitUntil(
+        () => $('.next-pagination-list').isExisting(),
+        {
+          timeout: 5000,
+          timeoutMsg: 'expected text to be different after 5s',
+        },
+    );
+
+    const nextButtons = $$('.next-pagination-list .next-btn');
+    nextButtons[1].click();
+    const listItems = $$('.list-item');
+    listItems[1].click();
+    browser.pause(3000);
+    const windowHandle = browser.getWindowHandles();
+    browser.switchToWindow(windowHandle[1]);
+    browser.pause(3000);
+    console.log($('.product-quantity-tip span').getText());
+  });
 });
 
 
